d2bcc159f889bd60a7a7b91284499bc31250d2e0
Ultra MechaKaiju <k1ohlarik@gmail.com>
2025-04-03 00:40:55 -0400

    Began the ground work for creating a character

--- testModule.py
+++ testModule.py
@@ -1,2 +1,38 @@
+from pickle import TRUE
+import random
+import pygame
+import sys
 
-print("test")
+pygame.init()
+clock = pygame.time.Clock()
+clock.tick(60)
+
+#screen settings
+screen = pygame.display.set_mode((1600,1200))
+pygame.display.set_caption("simple 2D game")
+
+#Text
+scoreFont = pygame.font.Font(None,50)
+
+#deault sprites
+class playerCharacter(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+      super().__init__(*groups)
+      #self.spriteSheet[] = 
+      self.hitbox = pygame.Rect((0,0),(100,500))
+      self.colorBlock = pygame.surface.Surface(size = (self.hitbox.height,self.hitbox.width),)
+      self.colorBlock.fill("Red")
+
+score = scoreFont.render("score:", False, "Green")
+
+characterSprite = playerCharacter()
+
+while TRUE:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.blit(characterSprite.colorBlock, (0,0))
+    screen.blit(score, (random.randrange(0,50),0))
+    pygame.display.update()
+  

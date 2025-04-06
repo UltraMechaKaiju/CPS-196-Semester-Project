79df21c00af997573434f03a7bef8f0313aa2c02
Ultra MechaKaiju <k1ohlarik@gmail.com>
2025-04-06 02:32:59 -0400

    Completed Camera And charactermovement Feature

--- testModule.py
+++ testModule.py
@@ -1,8 +1,6 @@
 
 import copy
 import random
-from tokenize import group
-from typing import Self
 import pygame
 import sys
 
@@ -32,9 +30,9 @@ class playerCharacter(pygame.sprite.Sprite):
       #self.spriteSheet[] = 
       #PPos: center of the characters hit box
       self.PPos = startLocation
-      self.hitbox = pygame.Rect((0,0),(100,50))
+      self.hitbox = pygame.Rect((0,0),(50,100))
       self.velocity = 0.0
-      self.colorBlock = pygame.surface.Surface(size = (self.hitbox.height,self.hitbox.width),)
+      self.colorBlock = pygame.surface.Surface(size = (self.hitbox.width,self.hitbox.height),)
       self.colorBlock.fill("Green")
 
 class worldObject(pygame.sprite.Sprite):
@@ -65,22 +63,20 @@ def packageWorldGroup(targetGroup):
 
 def moveWorldOffset(moveVec, targetCharacter: playerCharacter, currentScene: Scene):
     #calculates character offset and global offset(camera location) as well as character position
-    cameraOffset = pygame.Vector2(0,0)
-    characterOffset = pygame.Vector2(0,0)
 
     #Part 1: move the character
     targetCharacter.PPos += moveVec
     # apply world boundry check
     #Check for collisions with the left and top of the world
-    if targetCharacter.PPos.y <= targetCharacter.hitbox.width/2:
-        targetCharacter.PPos.y = targetCharacter.hitbox.width/2
-    if targetCharacter.PPos.x <= targetCharacter.hitbox.height/2:
-        targetCharacter.PPos.x = targetCharacter.hitbox.height/2
+    if targetCharacter.PPos.y <= targetCharacter.hitbox.height/2:
+        targetCharacter.PPos.y = targetCharacter.hitbox.height/2
+    if targetCharacter.PPos.x <= targetCharacter.hitbox.width/2:
+        targetCharacter.PPos.x = targetCharacter.hitbox.width/2
     #Check for collisions with the right and bottom of the world
-    if currentScene.worldExtents.x <= targetCharacter.PPos.x + targetCharacter.hitbox.height/2:
-        targetCharacter.PPos.x = currentScene.worldExtents.x - targetCharacter.hitbox.height/2
-    if currentScene.worldExtents.y <= targetCharacter.PPos.y + targetCharacter.hitbox.width/2:
-        targetCharacter.PPos.y = currentScene.worldExtents.y - targetCharacter.hitbox.width/2
+    if currentScene.worldExtents.x <= targetCharacter.PPos.x + targetCharacter.hitbox.width/2:
+        targetCharacter.PPos.x = currentScene.worldExtents.x - targetCharacter.hitbox.width/2
+    if currentScene.worldExtents.y <= targetCharacter.PPos.y + targetCharacter.hitbox.height/2:
+        targetCharacter.PPos.y = currentScene.worldExtents.y - targetCharacter.hitbox.height/2
 
 
     #Part2: given the characters position, which has accounted for the owrld boundry and world objects that may have obstructed movement, move the camera to its proper position
@@ -89,37 +85,27 @@ def moveWorldOffset(moveVec, targetCharacter: playerCharacter, currentScene: Sce
     #"where the camera is" is the opposite of how all world objects should be offset
 
     #modify camaera position in case the player is towards the top or lefft edge of the world border
+
+    
+    cameraOffset = pygame.Vector2(targetCharacter.PPos.x,targetCharacter.PPos.y)
+    characterOffset = pygame.Vector2(currentScene.cameraExtents.x/2, currentScene.cameraExtents.y/2)
+
     if targetCharacter.PPos.y <= currentScene.cameraExtents.y/2:
         characterOffset.y = targetCharacter.PPos.y
         cameraOffset.y = currentScene.cameraExtents.y/2
-    else:
-        characterOffset.y = currentScene.cameraExtents.y/2
-        cameraOffset.y = targetCharacter.PPos.y
-
     if targetCharacter.PPos.x <= currentScene.cameraExtents.x/2:
         characterOffset.x = targetCharacter.PPos.x
         cameraOffset.x = currentScene.cameraExtents.x/2
-    else:
-        characterOffset.x = currentScene.cameraExtents.x/2
-        cameraOffset.x = targetCharacter.PPos.x
-
     #modify camaera position in case the player is towards the bottom or right edge of the world border
 
-    if targetCharacter.PPos.y >= currentScene.worldExtents.y - currentScene.cameraExtents.y/2:
+    if (targetCharacter.PPos.y >= currentScene.worldExtents.y - (currentScene.cameraExtents.y/2)):
         characterOffset.y = currentScene.cameraExtents.y - (currentScene.worldExtents.y - targetCharacter.PPos.y)
         cameraOffset.y = (currentScene.worldExtents.y - (currentScene.cameraExtents.y / 2))
-    else:
-        characterOffset.y = currentScene.cameraExtents.y/2
-        cameraOffset.y = targetCharacter.PPos.y
-
-    if targetCharacter.PPos.x >= currentScene.worldExtents.x - currentScene.cameraExtents.x/2:
+    if (targetCharacter.PPos.x >= currentScene.worldExtents.x - (currentScene.cameraExtents.x/2)):
         characterOffset.x = currentScene.cameraExtents.x - (currentScene.worldExtents.x - targetCharacter.PPos.x)
         cameraOffset.x = (currentScene.worldExtents.x - (currentScene.cameraExtents.x / 2))
-    else:
-        characterOffset.x = currentScene.cameraExtents.x/2
-        cameraOffset.x = targetCharacter.PPos.x
 
-    #set equal to worldOffset and characterOffset
+    #set equal to worldOffset and characterOffset and PPos of player character
     return -cameraOffset, characterOffset - (pygame.Vector2((targetCharacter.hitbox.x/2),(targetCharacter.hitbox.y/2))), targetCharacter.PPos
 
 
@@ -133,7 +119,7 @@ score = scoreFont.render("score:", False, "Green")
 characterSprite = playerCharacter(pygame.Vector2(1920/2,1080/2), characterElements)
 firstObject = worldObject(pygame.Vector2(100,100), worldObjects)
 secondObject = worldObject(pygame.Vector2(200,200), worldObjects)
-testScene = Scene(pygame.Vector2(1920,1080), pygame.Vector2(250,250), pygame.Vector2(0,0), worldObject)
+testScene = Scene(pygame.Vector2(1920,1080), pygame.Vector2(500,500), pygame.Vector2(0,0), worldObject)
 
 worldOffset = pygame.Vector2()
 desiredMove = pygame.Vector2()
@@ -157,21 +143,21 @@ while True:
     if pressed[pygame.K_d]:
        desiredMove.x += 10
     if (desiredMove != pygame.Vector2(0,0)):
-        desiredMove = desiredMove.normalize() * 50
+        desiredMove = desiredMove.normalize() * 5
 
     #print(desiredMove * 100)
     worldOffset, testScene.characterOffset, characterSprite.PPos = moveWorldOffset(desiredMove, characterSprite, testScene)
-    print(characterSprite.PPos, worldOffset,testScene.characterOffset )
+    print(characterSprite.PPos, worldOffset,testScene.characterOffset)
     backgroundBlock = pygame.surface.Surface((screen.get_rect().width, screen.get_rect().height))
 
     backgroundBlock.fill("White")
 
     screen.blit(backgroundBlock, (0,0))
     
-    screen.blits(packageWorldGroup(worldObjects))
     #screen.blit(testImage, (0,0))
     screen.blit(testScene.cameraBlock, (0,0))
-    screen.blit(characterSprite.colorBlock, testScene.characterOffset)
+    screen.blits(packageWorldGroup(worldObjects))
+    screen.blit(characterSprite.colorBlock, pygame.Vector2((testScene.characterOffset.x - (characterSprite.hitbox.width/2)), (testScene.characterOffset.y - (characterSprite.hitbox.height/2))))
     screen.blit(score, (random.randrange(0,50),0))
     
     pygame.display.update()
